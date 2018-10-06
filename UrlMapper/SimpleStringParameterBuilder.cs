cd2d455dea09b54c06d5fc8396bd3984a46de28e
Sakul Jaruthanaset <miolynet@perfenterprise.com>
2018-10-06 15:52:00 +0700

    Fix more.

--- UrlMapper/SimpleStringParameterBuilder.cs
+++ UrlMapper/SimpleStringParameterBuilder.cs
@@ -7,6 +7,19 @@ namespace UrlMapper
         public ISimpleStringParameter Parse(string pattern)
         {
             // TODO: Need to implement this method.
+            return new xx();
+        }
+    }
+
+    public class xx : ISimpleStringParameter
+    {
+        public void ExtractVariables(string target, IDictionary<string, string> dicToStoreResults)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool IsMatched(string textToCompare)
+        {
             throw new System.NotImplementedException();
         }
     }
